fe3424d740a263ef6bdae20b88a9e3bbe30a5197
Tinamiru <sjf95@naver.com>
2022-06-24 18:28:59 +0900

    DDIT Web Study Project.

---
+++ src/main/java/net/okjsp/ajax/Proxy.java
@@ -0,0 +1,31 @@
+package net.okjsp.ajax;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.servlet.annotation.WebServlet;
+
+public class Proxy {
+	public static void main(String[] args) throws Exception {
+	}
+
+	public static String getSource(String string) throws MalformedURLException,
+			IOException {
+		URL url = new URL(string);
+		URLConnection yc = url.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				yc.getInputStream()));
+		String inputLine;
+		StringBuilder sb = new StringBuilder();
+		while ((inputLine = in.readLine()) != null) {
+			sb.append(inputLine).append("\n");
+		}
+		in.close();
+		return sb.toString();
+	}
+
+}
